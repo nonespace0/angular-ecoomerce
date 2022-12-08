ebfba9d34050b20fef0a9f8eb1315bde99b807c3
Aravinth Ram S <shivaaravinth@gmail.com>
2022-12-08 07:49:44 +0530

    login

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,29 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+import { AdminhomeComponent } from './adminhome/adminhome.component';
+import { CartComponent } from './cart/cart.component';
+import { CategoryaddComponent } from './categoryadd/categoryadd.component';
+import { LoginComponent } from './login/login.component';
+import { ProductsaddComponent } from './productsadd/productsadd.component';
+import { RegisterComponent } from './register/register.component';
+
+import { GuardsGuard } from './security/guards.guard';
+
+
+
+
+const routes: Routes = [
+  {path: '', redirectTo: 'home', pathMatch: 'full'},
+  {path: 'admin', component: AdminhomeComponent,children:[{path: 'add', component: CategoryaddComponent}]},
+  {path: 'productAdd', component:ProductsaddComponent},
+  {path: 'register', component:RegisterComponent},
+  {path: 'login', component:LoginComponent},
+  {path: 'cart', component:CartComponent}
+  
+  
+ 
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,48 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { HttpClientModule } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+
+import { RouterModule } from '@angular/router';
+import { AdminhomeComponent } from './adminhome/adminhome.component';
+import { CategoriesService } from './service/categorie.service';
+import { FormsModule } from '@angular/forms';
+import { CategoryaddComponent } from './categoryadd/categoryadd.component';
+import { ProductsaddComponent } from './productsadd/productsadd.component';
+import { RegisterComponent } from './register/register.component';
+import { LoginComponent } from './login/login.component';
+import { CartComponent } from './cart/cart.component';
+import { RegisterService } from './service/register.service';
+import { LoginService } from './service/login.service';
+import { GuardsGuard } from './security/guards.guard';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+     AdminhomeComponent,
+     CategoryaddComponent,
+     ProductsaddComponent,
+     RegisterComponent,
+     LoginComponent,
+     CartComponent
+ 
+
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    RouterModule,
+    HttpClientModule,
+    FormsModule
   ],
-  providers: [],
+  providers: [CategoriesService,
+              RegisterService,
+              LoginService,],
+             
+            
   bootstrap: [AppComponent]
 })
 export class AppModule { }
